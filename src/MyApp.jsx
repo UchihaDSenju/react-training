9e7a43ef58306f5bacb77f419677175a775ac001
Tariq <rafiqtariq19@gmail.com>
2026-01-31 17:44:06 +0530

    Tailwind css functionality added

---
+++ src/MyApp.jsx
@@ -0,0 +1,12 @@
+export default function MyApp() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold underline text-center text-red-400">
+        Welcome to MyApp!
+      </h1>
+      <p className="mt-4 text-gray-700">
+        This is a sample application using React and Tailwind CSS.
+      </p>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MyApp from './MyApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <MyApp />
   </StrictMode>,
 )
